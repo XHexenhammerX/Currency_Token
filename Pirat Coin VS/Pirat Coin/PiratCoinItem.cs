5d59ea4e28e27411d905336d315d654525e4b6c1
X_Hexenhammer_X <juergen.velsink@gmail.com>
2023-03-13 19:42:11 +0100

    working cs update

--- Pirat Coin VS/Pirat Coin/PiratCoinItem.cs
+++ Pirat Coin VS/Pirat Coin/PiratCoinItem.cs
@@ -1,81 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using Eco.Core.Items;
-using Eco.Gameplay.Blocks;
-using Eco.Gameplay.Components;
-using Eco.Gameplay.Components.Auth;
-using Eco.Gameplay.DynamicValues;
-using Eco.Gameplay.Economy;
-using Eco.Gameplay.Housing;
-using Eco.Gameplay.Interactions;
-using Eco.Gameplay.Items;
-using Eco.Gameplay.Modules;
-using Eco.Gameplay.Minimap;
-using Eco.Gameplay.Objects;
-using Eco.Gameplay.Players;
-using Eco.Gameplay.Property;
-using Eco.Gameplay.Skills;
-using Eco.Gameplay.Systems.TextLinks;
-using Eco.Gameplay.Pipes.LiquidComponents;
-using Eco.Gameplay.Pipes.Gases;
-using Eco.Gameplay.Systems.Tooltip;
-using Eco.Shared;
-using Eco.Shared.Math;
-using Eco.Shared.Localization;
-using Eco.Shared.Serialization;
-using Eco.Shared.Utils;
-using Eco.Shared.View;
-using Eco.Shared.Items;
-using Eco.Gameplay.Pipes;
-using Eco.World.Blocks;
-using Eco.Gameplay.Housing.PropertyValues;
-using Eco.Gameplay.Civics.Objects;
-using Eco.Gameplay.Settlements;
-using Eco.Gameplay.Systems.NewTooltip;
-using Eco.Core.Controller;
-using static Eco.Gameplay.Housing.PropertyValues.HomeFurnishingValue;
-using Eco.PiratCoin;
-using System.Reflection.Metadata.Ecma335;
-
-namespace Eco.PiratCoin
+﻿namespace Eco.PiratCoin
 {
-    [Serialized]
-    [RequireComponent(typeof(SolidAttachedSurfaceRequirementComponent))]
-    [RequireComponent(typeof(PropertyAuthComponent))]
-    [RequireRoomContainment]
-    public partial class PiratCoinItem : WorldObject
-    {
-        public override LocString DisplayDescription { get { return Localizer.DoStr("Pirat Coin"); } }
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Eco.Core.Items;
+    using Eco.Gameplay.Blocks;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.Components.Auth;
+    using Eco.Gameplay.DynamicValues;
+    using Eco.Gameplay.Economy;
+    using Eco.Gameplay.Housing;
+    using Eco.Gameplay.Interactions;
+    using Eco.Gameplay.Items;
+    using Eco.Gameplay.Modules;
+    using Eco.Gameplay.Minimap;
+    using Eco.Gameplay.Objects;
+    using Eco.Gameplay.Players;
+    using Eco.Gameplay.Property;
+    using Eco.Gameplay.Skills;
+    using Eco.Gameplay.Systems.TextLinks;
+    using Eco.Gameplay.Pipes.LiquidComponents;
+    using Eco.Gameplay.Pipes.Gases;
+    using Eco.Gameplay.Systems.Tooltip;
+    using Eco.Shared;
+    using Eco.Shared.Math;
+    using Eco.Shared.Localization;
+    using Eco.Shared.Serialization;
+    using Eco.Shared.Utils;
+    using Eco.Shared.View;
+    using Eco.Shared.Items;
+    using Eco.Gameplay.Pipes;
+    using Eco.World.Blocks;
+    using Eco.Gameplay.Housing.PropertyValues;
+    using Eco.Gameplay.Civics.Objects;
+    using Eco.Gameplay.Settlements;
+    using Eco.Gameplay.Systems.NewTooltip;
+    using Eco.Core.Controller;
+    using Eco.PiratCoin;
+    using System.Numerics;
 
+    [Serialized]
+    [RequireComponent(typeof(PropertyAuthComponent))]
+    [RequireComponent(typeof(SolidAttachedSurfaceRequirementComponent))]
+    [RequireComponent(typeof(RoomRequirementsComponent))]
+    [RequireRoomContainment]
+
+    public partial class PiratCoinObject : WorldObject, IRepresentsItem
+    {
+        public override LocString DisplayName { get { return Localizer.DoStr("Pirat Coin"); } }
+
+        public virtual Type RepresentedItemType { get { return typeof(PiratCoinItem); } }
+
+        protected override void Initialize() { }
+
+        static PiratCoinObject() { }
+
+        public override void OnPermanentDestroy()
+        {
+            base.OnPermanentDestroy();
+        }
+    }
+    [Serialized]
+    [Currency]
+    [MaxStackSize(50)]
+    [Weight(500)]
+    [LocDisplayName("Pirat Coin")]
+    public partial class PiratCoinItem : WorldObjectItem<PiratCoinObject>
+    {
+        public override LocString DisplayDescription { get { return Localizer.DoStr("A mystic coin "); } }
         public override DirectionAxisFlags RequiresSurfaceOnSides { get; } = 0
-                    | DirectionAxisFlags.Down;
-        
+                    | DirectionAxisFlags.Backward;// maybe Down for place it on the ground
         static PiratCoinItem()
         {
-            WorldObject.AddOccupancy<PiratCoinItem>(new List<BlockOccupancy>(){
+            WorldObject.AddOccupancy<PiratCoinObject>(new List<BlockOccupancy>(){
             new BlockOccupancy(new Vector3i(0, 0, 0)),
             new BlockOccupancy(new Vector3i(0, 1, 0)),
             });
         }
 
-
-    [Serialized]
-    [LocDisplayName("Pirat Coin")]
-    [Weight(500)]
-    [MaxStackSize(20)]
-    [Currency]
-    [Tag("Token")]
-    [Ecopedia("Items", "Products", createAsSubPage: true]
-    [Category(Hidden)]
-
-    public partial class PiratCoinItem : Item;
-    {
-        public override LocString DisplayDescription {get { return Localizer.DoStr("An ancient mystical coin that secures the wealth of the world"); } }
-
-static PiratCoinItem()
-{
-
-}
-            }
-
+    }
+}   
